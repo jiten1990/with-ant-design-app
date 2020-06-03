7a8661cd062cade0c082b29259259301d96fb1f2
jiten1990 <jiten@coronation.in>
2020-06-03 15:49:15 +0530

    Update index.js

--- pages/in-focus/index.js
+++ pages/in-focus/index.js
@@ -62,35 +62,29 @@ function Infocus({data, total, current_page}) {
 
 }
 
-Infocus.getInitialProps = () => {
+Infocus.getInitialProps = async ({query}) => {
 
-    // try {
-    //   let current_page = query.page;
-    //   let page = query.page ? (query.page-1) : 0;
-    //   let limit = 7;
-    //   let after  = base64.encode("arrayconnection:"+((page*limit)-1));
-    //   const allInFocusMain = await getPaginatedInFocus(after, limit);
-    //   const allInFocus = allInFocusMain.edges;
-    //   const allInFocusTotal = allInFocusMain.totalCount;
-    
-    //   return {
-    //     data: { allInFocus },
-    //     total : {allInFocusTotal },
-    //     current_page : {current_page}
-    //   }
-    // } catch (error) {
-    //   return {
-    //     data: { },
-    //     total : { },
-    //     current_page : { }
-    //   };
-    // }
-
-    return {
-      data: { },
-      total : { },
-      current_page : { }
-    };
+    try {
+      let current_page = query.page;
+      let page = query.page ? (query.page-1) : 0;
+      let limit = 7;
+      let after  = base64.encode("arrayconnection:"+((page*limit)-1));
+      const allInFocusMain = await getPaginatedInFocus(after, limit);
+      const allInFocus = allInFocusMain.edges;
+      const allInFocusTotal = allInFocusMain.totalCount;
+      return {
+        allInFocus
+        // data: { allInFocus },
+        // total : {allInFocusTotal },
+        // current_page : {current_page}
+      }
+    } catch (error) {
+      return {
+        // data: { },
+        // total : { },
+        // current_page : { }
+      };
+    }
     
 }
 
