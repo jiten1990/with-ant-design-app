0495907eefdf145789a954ffb819304ab0353f82
jiten1990 <jiten@coronation.in>
2020-06-03 16:37:52 +0530

    Update index.js

--- pages/in-focus/index.js
+++ pages/in-focus/index.js
@@ -3,58 +3,19 @@ import Router from 'next/router'
 import { useRouter } from 'next/router'
 import MasterLayout from '../../components/masterlayout'
 import { getPaginatedInFocus} from '../../lib/api'
-//import base64 from 'react-native-base64'
+import base64 from 'react-native-base64'
 import {Card, Pagination, Row, Col} from "antd"
 import { RichText } from 'prismic-reactjs'
 
-function Infocus({data, total, current_page}) {
+//function Infocus({data, total, current_page}) {
 
-  const router = useRouter();  
+const Infocus  = props => (
 
-  let allInFocus = data.allInFocus;
-  
-  function onChange(pageNumber) {
-    Router.push('/in-focus?page='+pageNumber).then(() => window.scrollTo(0, 0));
-  }
+  <MasterLayout>
+    <div>Testing</div> 
+  </MasterLayout>
 
-  if (!router.isFallback && !data) {
-    return <ErrorPage statusCode={404} />
-  }
-  else{
-    if(data){
-        return (
-          <MasterLayout>
-      
-              <p>Found {total.allInFocusTotal} records</p>
-      
-              <Card  title="In Focus" bordered={false}>
-              <Row>
-                {allInFocus.map(infocus => (
-                    <Col key={infocus.node._meta.id} span={8}>
-                      <div className="infocusListWrap">
-                        <div className="post-banner">
-                          <img alt={infocus.node.title} src={infocus.node.banner.url} />
-                        </div>
-                        <div>
-                          <RichText render={infocus.node.title} />
-                        </div>
-                      </div>  
-                    </Col>  
-                ))}        
-              </Row>
-              </Card>
-        
-              <Pagination onChange={onChange} defaultCurrent={current_page.current_page} total={total.allInFocusTotal} />  
-      
-          </MasterLayout>
-        )
-    }
-    else{
-      return ("Loading.....");
-    }
-  }
-
-}
+)
 
 Infocus.getInitialProps = async ({query}) => {
 
@@ -62,8 +23,8 @@ Infocus.getInitialProps = async ({query}) => {
     let current_page = query.page;
     let page = query.page ? (query.page-1) : 0;
     let limit = 7;
-    //let after  = base64.encode("arrayconnection:"+((page*limit)-1));
-    let after = "YXJyYXljb25uZWN0aW9uOjY=";
+    let after  = base64.encode("arrayconnection:"+((page*limit)-1));
+    //let after = "YXJyYXljb25uZWN0aW9uOjY=";
     const allInFocusMain = await getPaginatedInFocus(after, limit);
     const allInFocus = allInFocusMain.edges;
     const allInFocusTotal = allInFocusMain.totalCount;
@@ -76,7 +37,7 @@ Infocus.getInitialProps = async ({query}) => {
   } catch (error) {
     return {
       data: { },
-      total : { },
+      total : { },  
       current_page : { }
     };
   }
