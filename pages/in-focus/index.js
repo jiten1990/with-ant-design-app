011bb7fb95c45678385db2770ec6d045360d54d4
jiten1990 <jiten@coronation.in>
2020-06-03 16:51:32 +0530

    Update index.js

--- pages/in-focus/index.js
+++ pages/in-focus/index.js
@@ -1,98 +1,12 @@
-import React from 'react'
-import Router from 'next/router'
-import { useRouter } from 'next/router'
-import MasterLayout from '../../components/masterlayout'
-import { getPaginatedInFocus} from '../../lib/api'
-import base64 from 'react-native-base64'
-import {Card, Pagination, Row, Col} from "antd"
-import { RichText } from 'prismic-reactjs'
-
-function Infocus({data, total, current_page}) {
-
-  const router = useRouter();  
-
-  let allInFocus = [];
-
-  if(data.allInFocus){
-     allInFocus = data.allInFocus;
-  }
-  
-  function onChange(pageNumber) {
-    Router.push('/in-focus?page='+pageNumber).then(() => window.scrollTo(0, 0));
-  }
-
-  if (!router.isFallback && !data) {
-    return <ErrorPage statusCode={404} />
-  }
-  else{
-    if(data){
-        return (
-          <MasterLayout>
-      
-              <p>Found {total.allInFocusTotal} records</p>
-      
-              <Card title="In Focus" bordered={false}>
-              <Row>
-                {allInFocus.map(infocus => (
-                    <Col key={infocus.node._meta.id} span={8}>
-                      <div className="infocusListWrap">
-                        <div className="post-banner">
-                          <img alt={infocus.node.title} src={infocus.node.banner.url} />
-                        </div>
-                        <div>
-                          <RichText render={infocus.node.title} />
-                        </div>
-                      </div>  
-                    </Col>  
-                ))}        
-              </Row>
-              </Card>
-        
-              <Pagination onChange={onChange} defaultCurrent={current_page.current_page} total={total.allInFocusTotal} />  
-      
-          </MasterLayout>
-        )
-    }
-    else{
-      return ("Loading.....");
-    }
-  }
-
+function Page({ stars }) {
+  return <div>Next stars: {stars}</div>
 }
 
-Infocus.getInitialProps = ({query}) => {
+Page.getInitialProps = async (ctx) => {
+  const res = await fetch('https://api.github.com/repos/vercel/next.js')
+  const json = await res.json()
 
-  return {
-    data: { },
-    total : { },  
-    current_page : { }
-  };
-
-  // try {
-    
-  //   let current_page = query.page;
-  //   let page = query.page ? (query.page-1) : 0;
-  //   let limit = 7;
-  //   let after  = base64.encode("arrayconnection:"+((page*limit)-1));
-  //   //let after = "YXJyYXljb25uZWN0aW9uOjY=";
-  //   const allInFocusMain = await getPaginatedInFocus(after, limit);
-  //   const allInFocus = allInFocusMain.edges;
-  //   const allInFocusTotal = allInFocusMain.totalCount;
-  
-  //   return {
-  //     data: { allInFocus },
-  //     total : {allInFocusTotal },
-  //     current_page : {current_page}
-  //   }
-  // } catch (error) {
-  //   return {
-  //     data: { },
-  //     total : { },  
-  //     current_page : { }
-  //   };
-  // }
-
-  
+  return { stars: json.subscribers_count }
 }
 
-export default Infocus
+export default Page
