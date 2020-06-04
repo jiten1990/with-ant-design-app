ebb4d25ac1926fe267d6fda04cdf21c09ce87b9b
jiten1990 <jiten@coronation.in>
2020-06-04 17:50:54 +0530

    Update index.js

--- pages/in-focus/index.js
+++ pages/in-focus/index.js
@@ -19,33 +19,33 @@ function Infocus() {
 
 }
 
-// Infocus.getInitialProps = async ({query}) => {
+Infocus.getInitialProps = async ({query}) => {
 
-//   let current_page = query.page;
+  let current_page = query.page;
 
-//   try {
+  try {
     
-//     let page = query.page ? (query.page-1) : 0;
-//     let limit = 7;
-//     let after  = base64.encode("arrayconnection:"+((page*limit)-1));
-//     const allInFocusMain = await getPaginatedInFocus(after, limit);
-//     const allInFocus = allInFocusMain.edges;
-//     const allInFocusTotal = allInFocusMain.totalCount;
+    let page = query.page ? (query.page-1) : 0;
+    let limit = 7;
+    let after  = base64.encode("arrayconnection:"+((page*limit)-1));
+    const allInFocusMain = await getPaginatedInFocus(after, limit);
+    const allInFocus = allInFocusMain.edges;
+    const allInFocusTotal = allInFocusMain.totalCount;
   
-//     return {
-//       data: allInFocus ,
-//       total : allInFocusTotal ,
-//       current_page
-//     }
-//   } catch (error) {
-//     return {
-//       data: [],
-//       total : 0,
-//       current_page
-//     };
-//   }
+    return {
+      data: allInFocus ,
+      total : allInFocusTotal ,
+      current_page
+    }
+  } catch (error) {
+    return {
+      data: [],
+      total : 0,
+      current_page
+    };
+  }
 
   
-// }
+}
 
 export default Infocus
