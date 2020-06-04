0d0ca14356c6077efd54ccf7b0630eaa1fcfcd9a
jiten1990 <jiten@coronation.in>
2020-06-04 17:43:01 +0530

    Update index.js

--- pages/in-focus/index.js
+++ pages/in-focus/index.js
@@ -1,38 +1,89 @@
-//import fetch from 'isomorphic-unfetch'
-// import { Flex, Box } from 'reflexbox'
-//import { useRouter } from 'next/router'
-import { getAllKeyIssueTypes} from '../../lib/api'
-import {Card} from "antd"
+import ErrorPage from 'next/error'
+import React from 'react'
+import Router from 'next/router'
+import { useRouter } from 'next/router'
 import MasterLayout from '../../components/masterlayout'
-import Link from 'next/link'
+import { getPaginatedInFocus} from '../../lib/api'
+import base64 from 'react-native-base64'
+import {Card, Pagination, Row, Col} from "antd"
+import { RichText } from 'prismic-reactjs'
 
-function MoviesPage({ keyIssues }) {
-    
-    return (
-      <MasterLayout>
-        <Card title="Key Issues" bordered={false}>
-            
-          {keyIssues.map(issue => (
+function Infocus({data, total, current_page}) {
 
-            <p key={issue.node._meta.uid}>
-              <Link as={`/key-issues/${issue.node._meta.uid}`} href="/key-issues/[issue.node._meta.uid]">
-                <a className="hover:underline">
-                  {issue.node.category}
-                </a>
-              </Link>
-            </p>
-          ))}
+  const router = useRouter();  
 
-          </Card>
-      </MasterLayout>
-    )
-}
-
-export async function getServerSideProps({ query: {page= 1}, previewData }) {
-  const keyIssues = await getAllKeyIssueTypes(previewData)
-  return {
-    props: { keyIssues },
+  //let allInFocus = data.allInFocus;
+  
+  function onChange(pageNumber) {
+    Router.push('/in-focus?page='+pageNumber).then(() => window.scrollTo(0, 0));
   }
+
+  if (!router.isFallback && !data) {
+    return <ErrorPage statusCode={404} />
+  }
+  else{
+    if(data){
+        return (
+          <MasterLayout>
+      
+              <p>Found {total} records</p>
+      
+              <Card title="In Focus" bordered={false}>
+              <Row>
+                {data.map(infocus => (
+                    <Col span={8}>
+                      <div className="infocusListWrap">
+                        <div className="post-banner">
+                          <img alt={infocus.node.title} src={infocus.node.banner.url} />
+                        </div>
+                        <div>
+                          <RichText render={infocus.node.title} />
+                        </div>
+                      </div>  
+                    </Col>  
+                ))}        
+              </Row>
+              </Card>
+        
+              <Pagination onChange={onChange} defaultCurrent={current_page} total={total} />  
+      
+          </MasterLayout>
+        )
+    }
+    else{
+      return ("Loading.....");
+    }
+  }
+
 }
 
-export default MoviesPage
+Infocus.getInitialProps = async ({query}) => {
+
+  let current_page = query.page;
+
+  try {
+    
+    let page = query.page ? (query.page-1) : 0;
+    let limit = 7;
+    let after  = base64.encode("arrayconnection:"+((page*limit)-1));
+    const allInFocusMain = await getPaginatedInFocus(after, limit);
+    const allInFocus = allInFocusMain.edges;
+    const allInFocusTotal = allInFocusMain.totalCount;
+  
+    return {
+      data: allInFocus ,
+      total : allInFocusTotal ,
+      current_page
+    }
+  } catch (error) {
+    return {
+      data: [],
+      total : 0,
+      current_page
+    };
+  }
+
+  
+}
+
+export default Infocus
